05b8e381698834462ceb5653f812ba93106ef7a3
Melinda Diaz <meld8bitgamer@Melindas-MacBook-Pro.local>
2019-09-24 15:51:55 -0400

    added xcode project

---
+++ SwiftUIdemo/SwiftUIdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIdemo
+//
+//  Created by Melinda Diaz on 9/24/19.
+//  Copyright © 2019 Melinda Diaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
